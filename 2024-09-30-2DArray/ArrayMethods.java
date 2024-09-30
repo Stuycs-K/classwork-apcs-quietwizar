d880d79ec7af55a675f4639992fb5e46d6804fa6
Jai Shah <jaiS5@nycstudents.net>
2024-09-30 10:14:43 -0400

    Added new functions and attempted to debug issues, out of time

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,5 +1,8 @@
+//Jai Shah and Abdul Siddique
+//jaiS5@nycstudents.net and asiddique60@stuy.edu
+
 public class ArrayMethods{
-  public static String arrayToString(int[] nums){
+  public static String arrToString(int[] nums){
     String arr="[";
     if(nums.length==0){
       return "[]";
@@ -11,6 +14,15 @@ public class ArrayMethods{
     arr+="]";
     return arr;
   }
+
+  public static String arrToString(int[][]ary){
+    String arr="[";
+    for (int i = 0; i<ary.length; i++){
+      arr+=arrToString(ary[i]);
+    }
+    return "";
+  }
+
   public static int[] returnCopy(int[]ary){
     int[] arr=new int[ary.length];
     for(int i=0; i<ary.length; i++){
@@ -48,10 +60,13 @@ public class ArrayMethods{
     int[] test2 = new int[]{0, 4, 23,19, 23};
     int[] test3 = new int[]{1};
     int[] test4= new int[]{1, 12};
-    System.out.println("Original: [], Actual: "+ arrayToString(test1));
-    System.out.println("Original: [0, 4, 23, 19, 23], Actual: "+ arrayToString(test2));
-    System.out.println("Original: [1], Actual: "+ arrayToString(test3));
-    System.out.println("Original: [1,12], Actual: "+ arrayToString(test4));
+    int[] test5= new int[]{2, 3, 4, 9};
+    int[][] test6= new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}};
+
+    System.out.println("Original: [], Actual: "+ arrToString(test1));
+    System.out.println("Original: [0, 4, 23, 19, 23], Actual: "+ arrToString(test2));
+    System.out.println("Original: [1], Actual: "+ arrToString(test3));
+    System.out.println("Original: [1,12], Actual: "+ arrToString(test4));
 
     System.out.println("Does it match: "+ arraychecker(test1, returnCopy(test1)));
     System.out.println("Does it match: "+ arraychecker(test2,returnCopy(test2)));
@@ -61,6 +76,8 @@ public class ArrayMethods{
     System.out.println("Original: [] and [0, 4, 23, 19, 23], Concat: "+ arrayToString(concatArray(test1, test2)));
     System.out.println("Original: [0, 4, 23, 19, 23] and [1], Concat: "+ arrayToString(concatArray(test2, test3)));
     System.out.println("Original: [1] and [1,12], Concat: "+ arrayToString(concatArray(test3, test4)));
+    System.out.println("[2, 3, 4, 9] vs" + arrToString(test5));
+    System.out.println("[[2, 3, 4], [5, 6, 7], [2, 4, 9]]" + arraytest6);
 
 
 
