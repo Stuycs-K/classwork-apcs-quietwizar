390805bd096ab547e9a664dbf84c77a4ffc1885f
Jai Shah <jaiS5@nycstudents.net>
2024-10-07 10:09:27 -0400

    Added back the test cases as well

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -1,10 +1,44 @@
 import java.util.Arrays;
 public class ArrayDemo{
   public static void main(String[]args){
-    //write your tests here!
-    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
-    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
-    //do not use any other Arrays.method()
+    int[] test1 = new int[]{};
+    int[] test2 = new int[]{0, 4, 23,19, 23};
+    int[] test3 = new int[]{1};
+    int[] test4= new int[]{1, 12};
+    int[] test5= new int[]{2, 3, 4, 9};
+    int[][] test6= new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}};
+    int[][] emptytest=new int[][]{{}};
+    int[][] weirdtest=new int[][]{{3,7,4}, {2,1}, {}, {232}};
+    int[][] test7=new int[][]{{1,2,3},{4,5,6}};
+    int[][] test8 = new int[][]{{},{-2,-4,-6},{4,-5},{1,4,7,-8}};
+    int[][] test9=new int[][]{{},{3,7,-4,6},{12,21},{201,22,-100}};
+
+
+    System.out.println("Original: [], Actual: "+ arrToString(test1));
+    System.out.println("Original: [0, 4, 23, 19, 23], Actual: "+ arrToString(test2));
+    System.out.println("Original: [1], Actual: "+ arrToString(test3));
+    System.out.println("Original: [1,12], Actual: "+ arrToString(test4));
+
+    System.out.println("Does it match: "+ arraychecker(test1, returnCopy(test1)));
+    System.out.println("Does it match: "+ arraychecker(test2,returnCopy(test2)));
+    System.out.println("Does it match: "+ arraychecker(test3,returnCopy(test3)));
+    System.out.println("Does it match: "+ arraychecker(test4,returnCopy(test4)));
+
+    System.out.println("[2, 3, 4, 9] vs" + arrToString(test5));
+    System.out.println("Original: [[2, 3, 4], [5, 6, 7], [2, 4, 9]], new: " + arrToString(test6));
+    System.out.println("Array sum of [[2, 3, 4], [5, 6, 7], [2, 4, 9]]: "+ arr2DSum(test6));
+    System.out.println("Array sum of [[]]: "+ arr2DSum(emptytest));
+    System.out.println("Array sum of [[3,7,4], [2,1], [], [232]]: "+ arr2DSum(weirdtest));
+
+    System.out.println("Swapped of [[2,3,4],[5,6,7][2,4,9]]: "+ arrToString(swapRC(test6)));
+    System.out.println("Swapped of [[1,2,3],[4,5,6]]: "+ arrToString(swapRC(test7)));
+
+
+    replaceNegative(test8);
+    String CopyTest=arrToString(copy(test9));
+    System.out.println("Origional: {{},{3,7,-4,6},{12,21},{201,22,-100}}, copy= " + CopyTest + "and the copied string: " + arrToString(test9));
+    test9[1][2]=0;
+    System.out.println("Check copy chage: " + arrToString(test9) + " vs " + CopyTest);
 
   }
 
@@ -82,6 +116,14 @@ public class ArrayDemo{
   //DO NOT use any built in methods that "copy" an array.
   //You SHOULD write a helper method for this.
   //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[] returnCopy(int[]ary){
+    int[] arr=new int[ary.length];
+    for(int i=0; i<ary.length; i++){
+      arr[i]=ary[i];
+    }
+    return arr;
+  }
+
   public static int[][] copy(int[][] nums){
     int[][] copyarray=new int[nums.length][];
     for (int i=0; i<nums.length; i++){
