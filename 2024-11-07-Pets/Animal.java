985f62db0fc801328e831f0f53eac20832bee01c
Jai Shah <jais5@nycstudents.net>
2024-11-07 23:56:18 -0500

    Got the bird class constructer working

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -28,4 +28,6 @@ public class Animal {
   public String getNoise(){
     return noise;
   }
+  
 }
+
--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
     public static void main(String[] args){
         Animal dog= new Animal("woof", 10, "Good Dog");
         dog.speak();
+        Bird duck= new Bird ("tweet", 2, "the ugly duckling", 12.5, "white");
+        duck.speak();
     }
 }
