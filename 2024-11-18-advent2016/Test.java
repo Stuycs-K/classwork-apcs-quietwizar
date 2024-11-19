2cb6146078d1cfee23f37cd929fe34dff4d2c98b
Jai Shah <jais5@nycstudents.net>
2024-11-19 00:41:35 -0500

    Attempted a solution to d1 p2, not working

--- 2024-11-18-advent2016/Test.java
+++ 2024-11-18-advent2016/Test.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Test{
   public static int[] pathing(String filename){
@@ -10,11 +11,14 @@ public class Test{
   int Sud=0;
   int Ost=0;
   int West=0;
-  int[] changer={0,0,0,0};//North South East West
+  int[] changer={0,0,0,0,0};//North East South West
+  ArrayList<String> trackkeeper=new ArrayList<String>();
   try{
     File file=new File (filename);
     input = new Scanner(file);
     int holder=0;
+    int remember=0;
+    int holder2=0;
     while(input.hasNext()){
       String lines=input.next();
       if(lines.charAt(0)=='R'){
@@ -25,6 +29,12 @@ public class Test{
       }
       holder=(holder+4)%4;
       changer[holder]+=Integer.parseInt(lines.substring(1,lines.length()-1));
+      String location=(changer[0]-changer[2])+","+(changer[1]-changer[3]);
+      for(int j=0; j<trackkeeper.size();j++){
+        if(trackkeeper.get(j).equals(location)){
+          changer[4]=Math.abs(changer[0]-changer[2])+Math.abs(changer[1]-changer[3]);
+        }
+      }
       
     }
 
