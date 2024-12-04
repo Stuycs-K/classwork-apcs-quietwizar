e72458717b9baba88c9d4e78e99502d59f9cabf9
Jai Shah <jais5@nycstudents.net>
2024-12-04 01:20:51 -0500

    Added to color demo to make it more interesting

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,13 +1,27 @@
+import java.util.Random;
 public class ColorDemo{
-  public static void main(String[] args){
-    for(int r = 0; r < 256; r+=32){
-      for(int g = 0; g <= 256; g+=32){
-        for(int b = 0; b <= 256; b+=32){
-          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
-        }
+  public static void color(int r, int g, int b){
+    System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");  
+  }
+  public static void sleep(int milli){
+      try{
+              Thread.sleep(milli);
+      }catch(Exception e){
       }
-      System.out.print("\u001b[?25l");
-      System.out.println();
+  }
+  public static final String RESET="\u001b[0m";
+  public static void main(String[] args){
+    Random rand=new Random();
+    int randInt=rand.nextInt(257);
+    for(int i=0;i<256;i++){
+      int randInt1=rand.nextInt(257);
+      int randInt2=rand.nextInt(257);
+      int randInt3=rand.nextInt(257);
+      color(randInt1,randInt2,randInt3);
     }
+    System.out.print("\u001b[?25l");
+    System.out.println();
+    System.out.print(RESET);
   }
 }
+
