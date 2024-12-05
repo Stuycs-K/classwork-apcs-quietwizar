d3bf76f7c060322b916bbbd822d5a8a108e953af
Jai Shah <jais5@nycstudents.net>
2024-12-04 22:45:54 -0500

    Got a border working, moving to rest of lab

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -5,7 +5,14 @@ public class Driver{
         int[] made=new int[]{rand.nextInt(99),rand.nextInt(99),rand.nextInt(99)};
         return made;
     }
-    public static void bordermaker(int size, int length){
-        System.out.print()
+    public static void topborder(int length, String character, int color){
+        Text.color(color);
+        for(int i=0;i<length;i++){
+            System.out.print(character);
+        }
+    }
+    public static void main(String[] args){
+        topborder(80,"-",Text.RED);
+        System.out.println(Text.RESET);
     }
 }
