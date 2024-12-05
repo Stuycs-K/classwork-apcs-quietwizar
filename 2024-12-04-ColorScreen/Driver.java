c690883ce203e5d3b064e6cb77a2820c3665adb7
Jai Shah <jais5@nycstudents.net>
2024-12-04 22:34:22 -0500

    Added HW file and coded a helper function, dont know if it'll help but could be useful

---
+++ 2024-12-04-ColorScreen/Driver.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Driver{
+    public static int[] arrayMaker(){
+        Random rand=new Random();
+        int[] made=new int[]{rand.nextInt(99),rand.nextInt(99),rand.nextInt(99)};
+        return made;
+    }
+    public static void bordermaker(int size, int length){
+        System.out.print()
+    }
+}
