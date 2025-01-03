7ebdeba3e68f6ebb0fe1a5939da489a75e264c6d
Jai Shah <jaiS5@nycstudents.net>
2025-01-03 10:00:31 -0500

    Finished at home, added in class because I wasnt here 1/2

---
+++ 2025-01-02-Recursion/Fib.java
@@ -0,0 +1,16 @@
+public class fib{
+  public int fibonacci(int n) {
+    if(n==0){
+      return 0;
+    }
+    if(n==1){
+      return 1;
+    }
+    else{
+      return(fibonacci(n-1)+fibonacci(n-2));
+    }
+  }
+  public static void main(Strings[] args){
+    System.out.println(fib(4));
+  }
+}
